b02e3c3e40664d68cd86cacf65d9785a2999fa54
Matthew Sachs <matthewg@zevils.com>
2002-03-08 22:13:55 +0000

    Initial commit

---
+++ nntb/lib/NNTB/Weblog.pm
@@ -0,0 +1,25 @@
+package NNTB::Weblog;
+
+# This package provides common functions to all NNTB weblog modules.
+# NNTB modules should be inherit from this.
+
+$VERSION = '0.01';
+
+use strict;
+use vars qw($VERSION);
+use Carp;
+
+sub new($) {
+	my $class = ref($_[0]) || $_[0] || "NNTB::Weblog";
+	croak "Do not instantiate NNTB::Weblog directly; use one of its subclasses." if $class eq "NNTB::Weblog";
+
+	shift;
+	my $self = { };
+	bless $self, $class;
+
+	return $self;
+}
+
+
+
+1;
