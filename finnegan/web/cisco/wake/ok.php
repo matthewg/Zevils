c038b4447b66f4e21019f0ff02e365ccda37a7af
Matthew Sachs <matthewg@zevils.com>
2004-02-04 08:08:20 +0000

    Cisco enhancements

--- finnegan/web/cisco/wake/ok.php
+++ finnegan/web/cisco/wake/ok.php
@@ -5,16 +5,15 @@ require "../../include/finnegan.inc";
 
 $id = preg_replace("/[^0-9]/", "", $_REQUEST["id"]);
 
-$result = mysql_query("UPDATE wakes SET this_trigger_time=NULL, next_trigger_time=NULL WHERE extension='$extension' AND wake_id='$id'");
+$result = mysql_query("UPDATE wakes SET this_trigger_time=NULL, next_trigger_time=NULL WHERE extension='$extension' AND wake_id=$id");
 if(!$result) db_error();
-if(mysql_affected_rows() != 1) cisco_error("Alarm Not Found", "Alarm $id was not found.");
-mysql_query("UPDATE log_wake SET end_time=NOW, result='success' WHERE ISNULL(end_time) AND wake_id='$id' AND extension='$extension' AND event='activate'");
+mysql_query("UPDATE log_wake SET end_time=NOW, result='success' WHERE ISNULL(end_time) AND wake_id=$id AND extension='$extension' AND event='activate'");
 
 header("Refresh: 0; url=SoftKey:Exit");
 ?>
 
 <CiscoIPPhoneText>
-<Title>Done/Title>
+<Title>Done</Title>
 <Text>Exiting...</Text>
 </CiscoIPPhoneText>
 
--- finnegan/web/cisco/wake/snooze.php
+++ finnegan/web/cisco/wake/snooze.php
@@ -5,10 +5,9 @@ require "../../include/finnegan.inc";
 
 $id = preg_replace("/[^0-9]/", "", $_REQUEST["id"]);
 
-$result = mysql_query("UPDATE wakes SET next_trigger_time=NOW() + INTERVAL 9 MINUTE, this_trigger_time=NULL WHERE wake_id='$id'");
+$result = mysql_query("UPDATE wakes SET next_trigger_time=NOW() + INTERVAL 9 MINUTE, this_trigger_time=NULL WHERE wake_id=$id AND extension='$extension'");
 if(!$result) db_error();
-if(mysql_affected_rows() != 1) cisco_error("Alarm Not Found", "Alarm $id was not found.");
-mysql_query("UPDATE log_wake SET end_time=NOW, result='success', data='snooze' WHERE ISNULL(end_time) AND wake_id='$id' AND extension='$extension' AND event='activate'");
+mysql_query("UPDATE log_wake SET end_time=NOW, result='success', data='snooze' WHERE ISNULL(end_time) AND wake_id=$id AND extension='$extension' AND event='activate'");
 
 header("Refresh: 0; url=SoftKey:Exit");
 ?>
