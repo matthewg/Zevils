1ba8db6dc2d76c7844ac2a710f0bc43900f97918
Matthew Sachs <matthewg@zevils.com>
2004-02-05 12:20:19 +0000

    forgot_pin fix

--- finnegan/web/login.php
+++ finnegan/web/login.php
@@ -4,16 +4,6 @@ $page = "login";
 require "include/finnegan.inc";
 page_start();
 
-if($auth_ok) {
-	redirect("wakes.php");
-} else if($auth_error != "no_extension") {
-	if(isset($TEMPLATE["login"][$auth_error])) {
-		echo $TEMPLATE["login"][$auth_error];
-	} else {
-		echo $TEMPLATE["global"][$auth_error];
-	}
-}
-
 if(isset($_POST["op"])) {
 	$op = $_POST["op"];
 	if($op == "Forgot PIN" && $extension_ok && $extension) {
@@ -22,6 +12,14 @@ if(isset($_POST["op"])) {
 			echo $TEMPLATE["login"]["pin_not_found"];
 		else
 			echo $TEMPLATE["login"]["pin_sent"];
+	}
+} else if($auth_ok) {
+	redirect("wakes.php");
+} else if($auth_error != "no_extension") {
+	if(isset($TEMPLATE["login"][$auth_error])) {
+		echo $TEMPLATE["login"][$auth_error];
+	} else {
+		echo $TEMPLATE["global"][$auth_error];
 	}
 }
 
