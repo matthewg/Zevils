68e026a1aa0037522398c77142845b0d4da56766
Matthew Sachs <matthewg@zevils.com>
2004-04-07 23:15:11 +0000

    table def update

--- finnegan/misc/finnegan.sql
+++ finnegan/misc/finnegan.sql
@@ -1,62 +1,70 @@
-CREATE TABLE prefs (
-	extension CHAR(5) NOT NULL PRIMARY KEY,
-	pin VARCHAR(4) NULL,
-	forgot_pin BIT NOT NULL DEFAULT 0
-);
-
-CREATE TABLE wakes (
-	wake_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	extension CHAR(5) NOT NULL,
-	time TIME NOT NULL,
-	message INT NOT NULL DEFAULT 0,
-	date DATE NULL,
-	weekdays SET('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun') NULL,
-	cal_type SET('normal', 'holidays', 'Brandeis') NULL,
-	disabled BIT NOT NULL DEFAULT 0,
-	next_trigger_time DATETIME NULL,
-	this_trigger_time DATETIME NULL,
-	this_unreachable_count INT NOT NULL DEFAULT 0,
-	this_timeout_count INT NOT NULL DEFAULT 0,
-	this_post_count INT NOT NULL DEFAULT 0,
-	INDEX (next_trigger_time),
-	INDEX (this_trigger_time),
-	INDEX (this_unreachable_count),
-	INDEX (this_timeout_count),
-	INDEX (extension)
-);
-
-CREATE TABLE log_wake (
-	log_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	wake_id INT UNSIGNED NOT NULL,
-	extension CHAR(5) NOT NULL,
-	event ENUM('create', 'delete', 'edit', 'activate') NOT NULL,
-	result ENUM('success', 'failure') NULL,
-	start_time DATETIME NOT NULL,
-	end_time DATETIME NULL,
-	data VARCHAR(255) NULL,
-	phoneline VARCHAR(255) NULL,
-	ip VARCHAR(15) NULL,
-	INDEX (extension),
-	INDEX (wake_id)
+CREATE TABLE log_daemon (
+  log_id int(10) unsigned NOT NULL auto_increment,
+  time datetime NOT NULL default '0000-00-00 00:00:00',
+  data varchar(255) NOT NULL default '',
+  PRIMARY KEY  (log_id)
 );
 
 CREATE TABLE log_ext (
-	log_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	extension CHAR(5) NOT NULL,
-	event ENUM('auth', 'getwakes', 'setpin', 'delcookie', 'forgotpin') NOT NULL,
-	result ENUM('success', 'failure') NULL,
-	start_time DATETIME NOT NULL,
-	end_time DATETIME NOT NULL,
-	data VARCHAR(255) NULL,
-	phoneline VARCHAR(255) NULL,
-	ip VARCHAR(15) NULL,
-	INDEX (extension),
-	INDEX (event),
-	INDEX (start_time)
+  log_id int(10) unsigned NOT NULL auto_increment,
+  extension varchar(5) NOT NULL default '',
+  event enum('auth','getwakes','setpin','delcookie','forgotpin') NOT NULL default 'auth',
+  result enum('success','failure') default NULL,
+  start_time datetime NOT NULL default '0000-00-00 00:00:00',
+  end_time datetime NOT NULL default '0000-00-00 00:00:00',
+  data varchar(255) default NULL,
+  phoneline varchar(255) default NULL,
+  ip varchar(15) default NULL,
+  PRIMARY KEY  (log_id),
+  KEY extension (extension),
+  KEY event (event),
+  KEY result (result),
+  KEY start_time (start_time)
 );
 
-CREATE TABLE log_daemon (
-	log_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	time DATETIME NOT NULL,
-	data VARCHAR(255) NOT NULL
+CREATE TABLE log_wake (
+  log_id int(10) unsigned NOT NULL auto_increment,
+  wake_id int(10) unsigned NOT NULL default '0',
+  extension varchar(5) NOT NULL default '',
+  event enum('create','delete','edit','activate') NOT NULL default 'create',
+  result enum('success','failure') default NULL,
+  start_time datetime NOT NULL default '0000-00-00 00:00:00',
+  end_time datetime default NULL,
+  data varchar(255) default NULL,
+  phoneline varchar(255) default NULL,
+  ip varchar(15) default NULL,
+  PRIMARY KEY  (log_id),
+  KEY extension (extension),
+  KEY wake_id (wake_id),
+  KEY event (event),
+  KEY result (result),
+  KEY start_time (start_time)
+);
+
+CREATE TABLE prefs (
+  extension varchar(5) NOT NULL default '',
+  pin varchar(4) default NULL,
+  forgot_pin tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (extension)
+);
+
+CREATE TABLE wakes (
+  wake_id int(10) unsigned NOT NULL auto_increment,
+  extension char(5) NOT NULL default '',
+  time time NOT NULL default '00:00:00',
+  message int(11) NOT NULL default '0',
+  date date default NULL,
+  weekdays set('Mon','Tue','Wed','Thu','Fri','Sat','Sun') default NULL,
+  cal_type set('normal','holidays','Brandeis') default NULL,
+  snooze_count int(11) NOT NULL default '0',
+  trigger_date date default NULL,
+  trigger_snooze int(11) default NULL,
+  next_trigger datetime default NULL,
+  disabled tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (wake_id),
+  KEY time (time),
+  KEY date (date),
+  KEY weekdays (weekdays),
+  KEY next_trigger (next_trigger),
+  KEY extension (extension)
 );
