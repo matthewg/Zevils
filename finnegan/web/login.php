3b0fe813eb5e46098c2b29e9e2729a854f86a6c8
Matthew Sachs <matthewg@zevils.com>
2004-04-23 19:26:42 +0000

    More flexible forbidden_extensions syntax, validate extensions in Cisco service, don't generate PINs when using service

--- finnegan/web/login.php
+++ finnegan/web/login.php
@@ -8,11 +8,13 @@ if(isset($_POST["op"])) {
 	if($auth_ok) redirect("wakes.php");
 	$op = $_POST["op"];
 	if($op == "Forgot PIN" && $extension_ok && $extension) {
-		if(!mysql_query("UPDATE prefs SET forgot_pin=1 WHERE extension='$extension'")) db_error();
-		if(!mysql_affected_rows() && !mysql_num_rows(mysql_query("SELECT * FROM prefs WHERE extension='$extension'")))
-			echo $TEMPLATE["login"]["pin_not_found"];
-		else
-			echo $TEMPLATE["login"]["pin_sent"];
+		if(!$FinneganConfig->use_cisco || !$FinneganCiscoConfig->use_xml_service) {
+			if(!mysql_query("UPDATE prefs SET forgot_pin=1 WHERE extension='$extension'")) db_error();
+			if(!mysql_affected_rows() && !mysql_num_rows(mysql_query("SELECT * FROM prefs WHERE extension='$extension'")))
+				echo $TEMPLATE["login"]["pin_not_found"];
+			else
+				echo $TEMPLATE["login"]["pin_sent"];
+		}
 	}
 } else if($auth_ok) {
 	redirect("wakes.php");
