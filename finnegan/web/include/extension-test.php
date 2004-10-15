5b565300822a3f77e031756f410d5b4fb8182f55
Matthew Sachs <matthewg@zevils.com>
2004-10-15 20:00:33 +0000

    Strip newlines

--- finnegan/web/include/extension-test.php
+++ finnegan/web/include/extension-test.php
@@ -1,9 +1,9 @@
 <?
-
-$phonepage = implode("", @file("http://".$_SERVER["REMOTE_ADDR"]."/"));
+# nick: 10.11.16.188
+$phonepage = implode("", @file("http://10.11.16.188/"));
 if(!$phonepage) {
 	echo "Can't find extension\n";
-} else if(!preg_match("!Phone DN.*?<TD>(?:<[^>]*>)*[0-9]*([6-9][0-9]{4})\\b!mi", $phonepage, $matches)) {
+} else if(!preg_match("!Phone DN.*?<TD>(?:<[^>]*>)*[0-9]*([6-9][0-9]{4})\\b!si", $phonepage, $matches)) {
 	echo "Can't parse extension info\n";
 } else {
 	$extension = $matches[1];
