db8c37e0d168a60955bab8436b8eb92ed499a67c
Matthew Sachs <matthewg@zevils.com>
2007-11-25 14:19:18 +0000

    No PHP_AUTH

--- HTTPChallenge/HTTPChallenge.php
+++ HTTPChallenge/HTTPChallenge.php
@@ -14,15 +14,18 @@ function fnHTTPChallenge(&$user) {
     wfSetupSession();
 
     if(!isset($_REQUEST["auth"])) return true;
-
-    if(!isset($_SERVER['PHP_AUTH_USER'])) {
+    
+    if(!isset($_SERVER['HTTP_AUTHORIZATION'])) {
         header('WWW-Authenticate: Basic realm="'.$wgSiteName.'"');
         header('HTTP/1.1 401 Unauthorized');
-        die("Please log in, " . $_SERVER['PHP_AUTH_USER'] . "/" . $_SERVER['PHP_AUTH_PW']);
+        die("Please log in.");
     }
 
+    list($user, $pw) = explode(':',
+                               base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+    
     global $wgContLang;
-    $name = $wgContLang->ucfirst($_SERVER['PHP_AUTH_USER']);
+    $name = $wgContLang->ucfirst($user);
     $t = Title::newFromText($name);
     if(is_null($t)) return null;
 
@@ -32,7 +35,7 @@ function fnHTTPChallenge(&$user) {
 
     $u = User::newFromName($canonicalName);
     if(0 == $u->getID()) return null;
-    if(!$u->checkPassword($_SERVER['PHP_AUTH_PW'])) return null;
+    if(!$u->checkPassword($pw)) return null;
     $user = $u;
     $user->setCookies();
     
