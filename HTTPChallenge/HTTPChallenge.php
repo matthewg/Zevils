2ee0af4395027d030be6d4e335dcbb0c0e3bb35d
Matthew Sachs <matthewg@zevils.com>
2007-11-25 14:05:24 +0000

    checkpoint

---
+++ HTTPChallenge/HTTPChallenge.php
@@ -0,0 +1,41 @@
+<?php
+
+$wgExtensionCredits['other'][] = array(
+                                   'name' => 'HTTPChallenge',
+                                   'author' => 'Matthew Sachs',
+                                   'url' => 'http://zevils.com/',
+                                   'description' => 'Sends HTTP authentication challenge'
+                                   );
+$wgHooks['AutoAuthenticate'][] = 'fnHTTPChallenge';
+
+function fnHTTPChallenge(&$user) {
+    global $wgSiteName;
+    
+    wfSetupSession();
+
+    if(!isset($_REQUEST["auth"])) return true;
+
+    if(!isset($_SERVER['PHP_AUTH_USER'])) {
+        header('HTTP/1.1 404 Unauthorized');
+        die("Please log in.");
+    }
+
+    global $wgContLang;
+    $name = $wgContLang->ucfirst($_SERVER['PHP_AUTH_USER']);
+    $t = Title::newFromText($name);
+    if(is_null($t)) return null;
+
+    global $wgAuth;
+    $canonicalName = $wgAuth->getCanonicalName($t->getText());
+    if(!User::isValidUserName($canonicalName)) return null;
+
+    $u = User::newFromName($canonicalName);
+    if(0 == $u->getID()) return null;
+    if(!$u->checkPassword($_SERVER['PHP_AUTH_PW'])) return null;
+    $user = $u;
+    $user->setCookies();
+    
+    return true;
+}
+
+?>
