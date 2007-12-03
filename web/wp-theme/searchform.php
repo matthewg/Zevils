26a36035618c91fb9e5eb25a7fcbdcc01f45122f
Matthew Sachs <matthewg@zevils.com>
2007-12-03 00:32:35 +0000

    checkpoint

--- web/wp-theme/searchform.php
+++ web/wp-theme/searchform.php
@@ -1,3 +1,3 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-Search: <input type="search" value="<?php the_search_query(); ?>" name="s" id="s" />
+<div>Search: <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" /></div>
 </form>
