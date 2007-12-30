938310917a63c5caef56dcb691de927f3b64702f
Matthew Sachs <matthewg@zevils.com>
2007-12-30 07:16:36 +0000

    Add 404 page

--- web/wp-theme/404.php
+++ web/wp-theme/404.php
@@ -1,10 +1,8 @@
-<?php header("Status: 404 Not Found"); ?>
-<html>
-<head>
-<title>404 Not Found</title>
-<script type="text/javascript" src="/mint/mint.js.php"></script>
-</head>
-<body>
+<?php
+  header("Status: 404 Not Found");
+  get_header();
+?>
+        <div id="content" class="narrowcolumn">
 <h1>I know I put that page somewhere...</h1>
 <p>
 This website would lose its head if it wasn't screwed on, or so its
@@ -12,5 +10,6 @@ mother tells it.  Try <a href="/">starting over.</a>
 </p><p>
 If you believe this to be in error, email matthewg -at- zevils.com
 </p>
-</body>
-</html>
+        </div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
