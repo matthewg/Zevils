3dbf5537e49a969eb30a50e4da8da08bd9d844b6
Matthew Sachs <matthewg@zevils.com>
2011-02-19 22:31:01 +0000

    Checkpoint

--- misc/twmi/index.py
+++ misc/twmi/index.py
@@ -94,8 +94,9 @@ if access_token and access_token_secret:
                       access_token_secret=access_token_secret)
     get_friends(api)
 elif path == "/verified" and oauth_secret:
+    form = cgi.FieldStorage()
     get_access_token(oauth_secret=oauth_secret,
-                     oauth_token=cgi.FieldStorage.getfirst("oauth_token"),
-                     oauth_verifier=cgi.FieldStorage.getfirst("oauth_verified"))
+                     oauth_token=form.getfirst("oauth_token"),
+                     oauth_verifier=form.getfirst("oauth_verified"))
 else:
     get_request_token()
